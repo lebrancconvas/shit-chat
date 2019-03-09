f3558b815b349721128cbd4db05c8cc0bbead703
lebrancconvas <lebranccconvas@gmail.com>
2019-03-10 04:04:55 +0700

    :tada: Create Repository with NodeJS + Socket.io + Express.

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+const APP_PORT = 5555
+
+const server = app.listen(APP_PORT, () => {
+    console.log(`App running on port ${APP_PORT}`)
+})
+
+const io = require('socket.io').listen(server)
+
+// ตั้งค่า เพื่อให้ express ทำการ render view ที่โฟลเดอร์ views
+// และใช้ template engine เป็น pug
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+io.on('connection', (socket) => {
+    console.log('a user connected')
+    socket.on('chatter', (message) => {
+        console.log('chatter : ', message)
+        io.emit('chatter', message)
+    })
+})
